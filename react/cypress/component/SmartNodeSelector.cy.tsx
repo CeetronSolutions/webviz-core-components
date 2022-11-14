d08ecd9e25e52e5ec011b6f60e71aa58fad1f3fe
Ruben <ruben.thoms@ceetronsolutions.com>
2022-11-14 16:13:22 +0100

    Adjustments

---
+++ react/cypress/component/SmartNodeSelector.cy.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { mount } from "@cypress/react";
+
+describe("SmartNodeSelector.cy.ts", () => {
+    it("playground", () => {
+        mount(<div />);
+    });
+});
